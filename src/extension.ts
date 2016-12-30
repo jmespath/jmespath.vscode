3bdd9754d3912ff4fe34616eec4d8ba209ec8aa1
James Saryerwinnie <js@jamesls.com>
2016-12-29 20:34:55 -0800

    Initial commit of JMESPath extension for vscode
    You can search the current JSON document
    with a JMESPath expression.

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as jmespath from 'jmespath';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,9 +17,22 @@ export function activate(context: vscode.ExtensionContext) {
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
         // The code you place here will be executed every time your command is executed
+        //let jsonText = editor
+        let editor = vscode.window.activeTextEditor;
+        let jsonText = editor.document.getText();
+        let jsonDoc = JSON.parse(jsonText);
+        vscode.window.showInputBox({
+            placeHolder: '',
+            prompt: 'Enter a JMESPath expression'
+        }).then(value => {
+            let query = value;
+            let result = jmespath.search(jsonDoc, query);
+            let outputChannel = vscode.window.createOutputChannel('JMESPath');
+            outputChannel.clear();
+            outputChannel.append(JSON.stringify(result, null, 2));
+            outputChannel.show();
+        });
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
     });
 
     context.subscriptions.push(disposable);
